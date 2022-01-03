6cafc4f043ccd58cfe10c932a4c1497821f50d06
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-03 00:49:09 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+axios({
+    url: 'http://www.yourserver.com/api/neat_stuff',
+    method: 'post',
+    headers: {
+      'content-type': 'multipart/form-data'
+    },
+    data: form
+  })
+
+  // Endless scroll xkcd
+//    Trigger new API call on each scrolldown
+  // Put a counter on each API call / image load
+//   Menu bar: Random, by date, endless scroll, newest, oldest, NFT (just kidding donate)
+
+
