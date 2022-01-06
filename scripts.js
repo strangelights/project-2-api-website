72c7aafe603629a3217a3e15e1dd915094ada024
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-05 22:41:12 -0500

    Add new and best stories logic

--- scripts.js
+++ scripts.js
@@ -1,27 +1,52 @@
 // Declare these now and use later
-const main = document.body.querySelector('main');
 const topStories = [];
 const newStories = [];
 const bestStories = [];
 
-// Data fetching
+/* Fetch Hacker News Stories */
 const getTopStories = async () => {
     const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+    const topStoryIds = response.data; // Top 500 story IDs - 
     
-    const topStoryIds = response.data;
-    // Loop over array i # of times of ids and make individual API calls to retrieve data for each and add to new multidimensional array
-    for (let i = 0; i < 25; i++) {
-        let id = topStoryIds[i];
+    showStories(topStoryIds, topStories);
+}
+
+const getNewStories = async () => {
+    const response = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
+    const newStoryIds = response.data; // 500 Newest Story IDs
+    
+    showStories(newStoryIds, newStories);
+}
+
+const getBestStories = async () => {
+    const response = await axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
+    const bestStoryIds = response.data; // 500 Best story IDs
+
+    showStories(bestStoryIds, bestStories);
+}
+
+/* Reusable helper functions */
+ const showStories = async (idArray, storyArray, offset = 0) => {
+    // Loop over array 25 times at the specified offset to retrieve individual stories
+    for (let i = offset; i < 25; i++) {
+        let id = idArray[i];
         let story = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
         let title = story.data.title;
         let url = story.data.url;
-        topStories.push([title, url]);
+        storyArray.push([title, url]);
     }
+    
+    // Inject story content into the DOM
+    buildDOM(storyArray);
+}
 
-    // Inject story content into DOM
-    topStories.forEach((story, i) => {
+const buildDOM = (array) => {
+    // Creates necessary dom elements and populates values using function parameter
+    array.forEach((story, i) => {
+
+        let main = document.body.querySelector('main');
         let article = document.createElement('article');
-     
+        
         let rank = document.createElement('span');
         rank.className = "rank";
         rank.innerText = `${i + 1}. `;
@@ -38,16 +63,11 @@ const getTopStories = async () => {
 }
 
 getTopStories();
+getNewStories();
+getBestStories();
 
-const getNewStories = async () => {
-    const newStories = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
-    return newStories.data;
-}
 
-const getBestStories = async () => {
-    const bestStories = await axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
-    return bestStories.data;
-}
+
 
 
 
